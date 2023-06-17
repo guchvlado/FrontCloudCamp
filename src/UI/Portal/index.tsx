7925af1d0f6ee84d98728755a2372470553bc1e1
Guchenko Vladislav <guchvlado324@gmail.com>
2023-06-17 15:32:59 +0300

    fix: Portal children fix

---
+++ src/UI/Portal/index.tsx
@@ -0,0 +1,22 @@
+import { PropsWithChildren, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+export interface PortalProps {
+  target?: HTMLElement;
+}
+
+export const Portal: React.FC<PropsWithChildren<PortalProps>> = ({ children, target = document.body }) => {
+  const portalRef = useRef(document.createElement("div"));
+
+  useEffect(() => {
+    target?.appendChild(portalRef.current);
+
+    const node = portalRef.current;
+
+    return () => {
+      target?.removeChild(node);
+    };
+  }, []);
+
+  return ReactDOM.createPortal(children, portalRef.current);
+};
