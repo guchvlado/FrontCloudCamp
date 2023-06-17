f1c19191e0db5eb4fdfde3a0873a4f3a27024472
Guchenko Vladislav <guchvlado324@gmail.com>
2023-06-17 17:14:24 +0300

    feat: MessageModal component

---
+++ src/components/MessageModal/index.tsx
@@ -0,0 +1,59 @@
+
+
+import { Button, Modal } from '../../UI'
+import cn from 'classnames'
+import styles from './index.module.scss'
+
+import SuccessIcon from '../../assets/messageSuccess.svg'
+import ErrorIcon from '../../assets/messageError.svg'
+import CloseIcon from '../../assets/closeIcon.svg'
+import { Link, useNavigate } from 'react-router-dom'
+
+export interface MessageModalProps {
+    opened: boolean;
+    onClose: () => void;
+    isSuccess?: boolean
+    isError?: boolean
+  }
+
+export const MessageModal: React.FC<MessageModalProps> = ({onClose, opened, isError, isSuccess}) => {
+    if (!isError && !isSuccess) return null;
+
+    return (
+        <Modal onClose={onClose} opened={opened} closeOnClickOutside className={styles.modal} >
+            {isSuccess ? <SuccessContent/> : isError ? <ErrorContent onClose={onClose}/> : null}
+        </Modal>
+    )
+}
+
+const SuccessContent = () => {
+    
+    const navigate = useNavigate()
+
+    const onClickButton = () => {
+        navigate('/')
+    }
+
+    return (
+        <>
+            <div className={styles.text}>Форма успешно отправлена</div>
+            <img className={styles.icon} src={SuccessIcon} alt="Success" />
+            <Button className={cn(styles.button, styles.success)} id='button-to-main' onClick={onClickButton}>На главную</Button>
+        </>
+    )
+}
+
+const ErrorContent = ({onClose}: {onClose: () => void}) => {
+    return (
+        <>
+            <div className={styles.header}>
+                <div className={styles.text}>Ошибка</div>
+                <div className={styles.close} onClick={onClose}>
+                    <img src={CloseIcon} alt="close" />
+                </div>
+            </div>
+            <img className={styles.icon} src={ErrorIcon} alt="Error" />
+            <Button className={cn(styles.button, styles.error)} id='button-close' onClick={onClose}>Закрыть</Button>
+        </>
+    )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export * from './ContactItem'
 export * from './StepOne'
+export * from './StepTwo'
+export * from './StepThree'
+export * from './MessageModal'
