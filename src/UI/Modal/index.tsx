00674634a64999512495cff776d9d287dd59d235
Guchenko Vladislav <guchvlado324@gmail.com>
2023-06-17 15:33:16 +0300

    feat: Portal UI

---
+++ src/UI/Modal/index.tsx
@@ -0,0 +1,38 @@
+import { Portal } from "../Portal"
+import { PropsWithChildren, useRef } from "react"
+import styles from "./index.module.scss"
+import cn from "classnames"
+
+export interface ModalProps {
+  className?: string
+  opened: boolean
+  onClose: () => void
+  closeOnClickOutside?: boolean
+}
+
+export const Modal: React.FC<PropsWithChildren<ModalProps>> = ({ children, className, opened, onClose, closeOnClickOutside }) => {
+
+  const modalClass = cn(styles.modal, className)
+
+  const modalRef = useRef<HTMLDivElement>(null)
+
+  const handleOverlayClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === modalRef.current && closeOnClickOutside) {
+      onClose()
+    }
+  }
+
+  if (!opened) {
+    return null
+  }
+
+  return (
+    <Portal>
+      <div className={styles.overlay} onClick={handleOverlayClick} ref={modalRef}>
+        <div className={modalClass}>
+          {children}
+        </div>
+      </div>
+    </Portal>
+  )
+}
