0a3b836d70ca0c109633e94f82fb64858a69454e
Guchenko Vladislav <guchvlado324@gmail.com>
2023-06-17 14:02:35 +0300

    feat: TextArea UI

---
+++ src/UI/TextArea/index.tsx
@@ -0,0 +1,28 @@
+import { InputHTMLAttributes, forwardRef, DetailedHTMLProps, TextareaHTMLAttributes } from "react";
+import cn from "classnames";
+import styles from './index.module.scss'
+
+import { TextAreaResize } from './TextAreaResize';
+
+interface InputProps extends DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
+    label?: string;
+    error?: string;
+    counter?: boolean;
+}
+
+export const TextArea = forwardRef(({ className, id, label, counter = false, error, value, ...props}: InputProps, forwardRef) => {
+    const textAreaClass = cn(styles.textarea, className);
+
+    return (
+        <div className={styles.root}>
+            {label ? <label htmlFor={id} className={styles.label}>{label}</label> : null}
+            <div className={styles.container}>
+                <TextAreaResize className={styles.icon} />
+                <textarea value={value} id={id} className={textAreaClass} {...props}/>
+            </div>
+            {counter && <div className={styles.counter}>Кол-во символов {value ? value.toString().length : 0}</div>}
+            {error ? <p className={styles.error}>{error}</p> : null}
+            
+        </div>
+    )
+})
